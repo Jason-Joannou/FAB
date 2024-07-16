0a7cc7a84e49a95e4b3c5e122f9b6a209e1141c9
Jason Joannou <jjoannou@bscglobal.com>
2024-07-16 08:43:05 +0200

    Can take multiple arguments

--- fab/fab.py
+++ fab/fab.py
@@ -1,9 +1,7 @@
 import argparse
 import os
 import sys
-
 from .venv_types import run_venv
-
 
 def create_virtual_environment():
     user_response = input(
@@ -11,38 +9,39 @@ def create_virtual_environment():
     ).lower()
     return user_response == "yes"
 
-
 def create_and_run_virtual_environment(project_path, venv_name, generate_report):
     run_venv.run_fab(project_path=project_path, venv_name=venv_name, generate_report=generate_report)
 
-
 def run_fab_and_save_results(project_path, venv_name, generate_report):
     create_and_run_virtual_environment(project_path=project_path, venv_name=venv_name, generate_report=generate_report)
-    print("Those looks like some FABulous suggestions!")
-
+    print(f"Processing project: {project_path}\nThose look like some FABulous suggestions!")
 
 def main():
     parser = argparse.ArgumentParser(
         description="Run fab commands in a Python project."
     )
     parser.add_argument(
-        "--project_path", help="Path to the Python project", required=True
+        "--project_paths", 
+        help="Path(s) to the Python project(s)", 
+        required=True, 
+        nargs='+'
     )
     parser.add_argument(
         "--venv_name",
-        help="Virtual Enviroment Name within the project. Defaults to format_venv",
+        help="Virtual Environment Name within the project. Defaults to format_venv",
         required=False,
         default="format_venv",
     )
     parser.add_argument("--generate_report", action="store_true", help="Generate formatting report", default=False)
 
     args = parser.parse_args()
-    project_path = args.project_path
+    project_paths = args.project_paths
     generate_report = args.generate_report
 
     venv_name = args.venv_name
-    run_fab_and_save_results(project_path, venv_name, generate_report)
 
+    for project_path in project_paths:
+        run_fab_and_save_results(project_path, venv_name, generate_report)
 
 if __name__ == "__main__":
     main()
